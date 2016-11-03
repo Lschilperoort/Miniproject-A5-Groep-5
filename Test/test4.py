748416ecd93364488afced950d23622655d6607b
Lschilperoort <lucas.schilperoort@student.hu.nl>
2016-11-03 20:13:13 +0100

    Filler

--- Test/test4.py
+++ Test/test4.py
@@ -1,16 +1,13 @@
 from tkinter import *
+root = Tk()
 
+def additem():
+    global items
+    items += 1
+    b.configure(text="I have %s items" % (items))
 
-root=Tk()
-
-w = Label(root, text="Rouge", fg="red")
-w = Label(root, text="Helvetica", font=("Helvetica", 16))
-w.pack()
-w.place(x=20, y= 20)
-
-
+items = 14
+b = Button(root, text="I have %s items" % (items), command=additem)
+b.pack()
 
 root.mainloop()
-
-
-tweet = '342i94324309249302483092830928324093283209389438092432' + '\n'
